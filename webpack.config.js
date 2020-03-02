ce8327ec39c6141cb97e61b4fc53cc30efc2a267
Hila Aurel <h.aurel97@yahoo.com>
2020-03-02 20:38:35 +0200

    menu changes

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,14 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader'
+                    {
+                        loader: "file-loader",
+                        options: { 
+                            outputPath: "img/",
+                            name: '[name].[ext]',
+                            //name: '[sha512:hash:base64:7].[ext]' 
+                        }
+                    }
                 ]
             },
             {
