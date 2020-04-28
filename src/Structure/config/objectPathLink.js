ca0219891807a6bbdaddb05df6d1e76e4fb84178
Hila Aurel <h.aurel97@yahoo.com>
2020-04-28 13:41:38 +0300

    path to object

---
+++ src/Structure/config/objectPathLink.js
@@ -0,0 +1,9 @@
+
+export default{
+    chair_001: '/src/Structure/Chair/chair_001.gltf',
+    chair_004: '/src/Structure/Chair/chair_004.gltf',
+    chair_006: '/src/Structure/Chair/chair_006.gltf',
+    chair_007: '/src/Structure/Chair/chair_007.gltf',
+    masa_picnic: '/src/Structure/Tables/masa_picnic.gltf',
+    masa_001: '/src/Structure/Tables/masa_001.gltf',
+}
